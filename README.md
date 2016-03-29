753ac9dfc328d86690be25f5bac8d2c367dae782
Dominic Zirbel <djynth@gmail.com>
2016-03-29 14:45:39 -0400

    added documentation for teachers and students

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 An implentation of the Infection project for Khan Academy's application process. The official description of the parameters can be found [here](https://docs.google.com/a/khanacademy.org/document/d/1NiKv-MjULOFyyc8f5w8R_EqvuPJ10wJVJgZhtTK9VKc/edit#heading=h.24vvz52659j3). In brief, the challenge is to implement a graph of students and teachers who can be "infected" classroom-by-classroom by a new version of a piece of software. Classrooms are infected as a whole to make sure classmates are using the same version, and in the Limited Infection case, the goal is to infect only a specified potion of the population.
 
-# Implementation
+#### Implementation
 
 I chose to complete this project in Python both because the majority of my portfolio is in a web-focused language (JavaScript, PHP, etc.) and so I wanted to highlight my ability in other environments, and because Python is high level enough to focus on the conceptual parts of the project and easily expand to, for example, a graphical interface.
 
-# Algorithm
+#### Algorithm
 
 The Limited Infection scenario is an example of the famous Subset Sum problem, where the goal is to choose a subset of a given set of numbers that sum to a certain target. If the goal is to infect `k` students and we have classrooms of sizes `a_1, a_2, a_3, ..., a_n` then we attempt to find a subset of these classrooms with size summing to `k`. The Subset Sum problem is known to be NP-complete, so a perfect polynomial time algorithm is beyond the scope of this project. Thus, we can either compromise on complexity (and implement a brute force, exponential time algorithm) and/or correctness (and implement an algorithm that finds a subset of classrooms with only approximately `k` total students).
--- infection.py
+++ infection.py
@@ -1,46 +1,83 @@
 class Teacher:
+    """Holds a typical classroom with a single Teacher and many Students."""
+
     def __init__(self, num_students = 0):
+        """Creates a new, uninfected classroom.
+
+        @param num_students : the number of students in the classroom, default 0
+        """
         self._students = [Student for i in xrange(num_students)]
         self._infected = False;
 
     @property
     def num_students(self):
+        """The number of students in this classroom (not including the 
+           teacher)."""
         return len(self._students)
 
     @property
     def num_users(self):
+        """The number of total users in this classroom (including the 
+           teacher)."""
         return 1 + self.num_students
 
     @property
     def num_infected(self):
+        """The number of infected users in this classroom (including the
+           teacher)."""
         return self.infected + sum(student.infected for student in students)
 
     @property
     def infected(self):
+        """Whether the teacher of this classroom has been infected.
+
+        Typically, but not always, students will be infected when the teacher
+            is.
+        """
         return self._infected
 
     def infect(self, infect = True, spread = True):
+        """Infects (or uninfects) this classroom.
+
+        @param infect : whether to toggle the infection on or off, default True
+        @param spread : whether to spread the infection to all the students in
+                        the classroom, default True
+        """
         self._infected = infect
         if spread:
             for student in students:
-                student.infect(infect)
+                student.infect(infect, False)
 
     def __str__(self):
-        return "[{}/{}]".format(self.num_infected(), self.num_users())
+        return "[{}/{}]".format(self.num_infected, self.num_users)
 
 class Student:
     def __init__(self, teacher, spread = True):
+        """Creates a new student in the given classroom.
+
+        @param teacher : the Teacher of this student
+        @param spread : whether to automatically infect this student if the
+                        teacher is infected, default True
+        """
         self._teacher = teacher
         self._infected = spread and teacher.infected
 
     @property
     def infected(self):
+        """Whether this student is infected."""
         return self._infected
 
     def infect(self, infect = True, spread = True):
+        """Infects this student, typically spreading the infection to its
+           classroom.
+
+        @param infect : whether the toggle the infection on or off, default True
+        @param spread : whether to spread the infection to all the the rest of
+                        the classroom, default True
+        """
         self._infected = infect
         if spread:
-            self._teacher.infect(infect)
+            self._teacher.infect(infect, True)
 
 
     
