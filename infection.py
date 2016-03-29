bd95b45ae5d465e564d021c142701300c1bf5c04
Dominic Zirbel <djynth@gmail.com>
2016-03-29 14:20:07 -0400

    added some utilities to the teacher

--- infection.py
+++ infection.py
@@ -3,8 +3,17 @@ class Teacher:
         self._students = [Student for i in xrange(num_students)]
         self._infected = False;
 
+    @property
     def num_students(self):
         return len(self._students)
+
+    @property
+    def num_users(self):
+        return 1 + self.num_students
+
+    @property
+    def num_infected(self):
+        return self.infected + sum(student.infected for student in students)
 
     @property
     def infected(self):
@@ -16,10 +25,13 @@ class Teacher:
             for student in students:
                 student.infect(infect)
 
+    def __str__(self):
+        return "[{}/{}]".format(self.num_infected(), self.num_users())
+
 class Student:
     def __init__(self, teacher, spread = True):
         self._teacher = teacher
-        self._infected = spread if teacher.infected else False
+        self._infected = spread and teacher.infected
 
     @property
     def infected(self):
@@ -29,4 +41,6 @@ class Student:
         self._infected = infect
         if spread:
             self._teacher.infect(infect)
+
+
     
